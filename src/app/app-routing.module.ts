3255a09704cc9542958c480c4a8d8fff3c511797
Lucas Peixoto <lspeixoto@stefanini.com>
2021-08-17 08:36:02 -0300

    Estruturação inicial dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { WelcomeComponent } from './auth/welcome/welcome.component';
+import { TrainingComponent } from './training/training.component';
 
-const routes: Routes = [];
+const AppRoutes: Routes = [
+	{ path: '', component: WelcomeComponent },
+	{ path: 'signup', component: SignupComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'training', component: TrainingComponent },
+];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(AppRoutes)],
 	exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/modules/material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { WelcomeComponent } from './auth/welcome/welcome.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [
+		AppComponent,
+		SignupComponent,
+		TrainingComponent,
+		CurrentTrainingComponent,
+		NewTrainingComponent,
+		PastTrainingComponent,
+		WelcomeComponent,
+	],
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
