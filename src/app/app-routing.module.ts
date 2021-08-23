2732c5ab61f3ee3d19cb6d7cb00d18cb39f5c955
Lucas Peixoto <lspeixoto@stefanini.com>
2021-08-23 08:50:04 -0300

    Inclusão do authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { WelcomeComponent } from './auth/welcome/welcome.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 import { TrainingComponent } from './training/training.component';
 
 const AppRoutes: Routes = [
 	{ path: '', component: WelcomeComponent },
 	{ path: 'signup', component: SignupComponent },
 	{ path: 'login', component: LoginComponent },
-	{ path: 'training', component: TrainingComponent },
+	{ path: 'training', component: TrainingComponent, canActivate: [AuthGuard] },
+  {path: '**', component: LoginComponent}
 ];
 
 @NgModule({
