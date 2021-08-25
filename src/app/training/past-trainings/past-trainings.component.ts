e77b16afb6ce1160273c250fb4ae004c77479ecf
Lucas Peixoto <lspeixoto@stefanini.com>
2021-08-24 22:00:24 -0300

    Inclusão da ordenação na tabela

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { TrainingService } from 'src/app/shared/services/training.service';
 import { Exercise } from '../../shared/models/exercise.model';
@@ -8,14 +9,20 @@ import { Exercise } from '../../shared/models/exercise.model';
 	templateUrl: './past-trainings.component.html',
 	styleUrls: ['./past-trainings.component.css'],
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
 	displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
 	dataSource = new MatTableDataSource<Exercise>();
+
+	@ViewChild(MatSort) sort: MatSort;
 
 	constructor(private trainingService: TrainingService) {}
 
 	ngOnInit(): void {
 		this.dataSource.data =
 			this.trainingService.getCompletedOrCancelledExercises();
+	}
+
+	ngAfterViewInit(): void {
+		this.dataSource.sort = this.sort;
 	}
 }
