b542fb51e861bdd2e8873cc802a12e4213f249b5
Lucas Peixoto <lspeixoto@stefanini.com>
2021-08-26 23:38:24 -0300

    Inclusão do AuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/modules/material.module';
-import { SignupComponent } from './auth/signup/signup.component';
+
 import { TrainingComponent } from './training/training.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './auth/welcome/welcome.component';
-import { LoginComponent } from './auth/login/login.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
 import { AuthService } from './shared/services/auth.service';
 import { TrainingService } from './shared/services/training.service';
 
-// Firebase
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
 import { LoadingComponent } from './shared/components/loading/loading.component';
 import { UiService } from './shared/services/ui.service';
+import { AuthModule } from './shared/modules/auth.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		SignupComponent,
-		LoginComponent,
 		TrainingComponent,
 		CurrentTrainingComponent,
 		StopTrainingComponent,
@@ -48,11 +43,11 @@ import { UiService } from './shared/services/ui.service';
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
-		FormsModule,
-		ReactiveFormsModule,
 		AppRoutingModule,
 		MaterialModule,
 		FlexLayoutModule,
+
+		AuthModule,
 
 		AngularFireModule.initializeApp(environment.firebase),
 		AngularFireDatabaseModule,
